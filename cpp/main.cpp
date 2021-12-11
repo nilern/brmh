59e22b36ebd09b021382f29dcbc21040e3095859
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-12-11 11:47:19 +0200

    Enable reading from stdin

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <optional>
 
 #include "lexer.cpp"
@@ -6,13 +7,21 @@
 #include "pos.cpp"
 
 int main (int argc, char** argv) {
-    if (argc > 1) {
-        brmh::Lexer tokens(brmh::Filename::create("<CLI arg>"), argv[1]);
+    if (argc == 2) {
+        std::string input;
+        if (strcmp(argv[1], "-") == 0) {
+            std::stringstream ss;
+            ss << std::cin.rdbuf();
+            input = ss.str();
+        } else {
+            input = argv[1];
+        }
 
+        brmh::Lexer tokens(brmh::Filename::create("<CLI arg>"), input.c_str());
         while (true) {
-            const auto c = tokens.next();
-            if (c) {
-                c.value().print(std::cout);
+            const auto tok = tokens.next();
+            if (tok) {
+                tok.value().print(std::cout);
                 std::cout << std::endl;
             } else {
                 break;
@@ -21,6 +30,7 @@ int main (int argc, char** argv) {
 
         return EXIT_SUCCESS;
     } else {
+        std::cerr << "Invalid CLI arguments" << std::endl;
         return EXIT_FAILURE;
     }
 }
